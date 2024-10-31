1d932f1883e1b634a5cea6a1f2cf71f5b514f663
choseongchan3712 <choseongchan3712@naver.com>
2024-10-31 15:00:03 +0900

    api

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Work list
 
-- [] Router 설정
+- [X] Router 설정
 - [] 각 폴더, 파일등 구성
 - [] api 설정
 - [] Loading, Header, Footer 글로벌 콤포넌트 구성
@@ -13,6 +13,7 @@
 # 설치항목
 
 - [x] npm i react-router-dom
+- [x] npm install node-fetch@2
 - [] npm i styled-components
 - [] npm i styled-reset
 - [] swiper
---
+++ src/api.js
@@ -0,0 +1,27 @@
+import fetch from "node-fetch";
+// const fetch = require("node -fetch"); 이렇게도 가능 함
+
+const baseUrl = "https://api.themoviedb.org/3/";
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNjgxNDNhYWZhNzlmNzBiODg5NDdiYzZhOThhNGNjMCIsIm5iZiI6MTczMDI2MzAyOS42MjAxNDg0LCJzdWIiOiI2NzIxYjQ1NTgyNmZlNTc5OWNjNGE3NmQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.7q59cSNpTBjE4_OUirV4gRcERwvhXu1h1fgbg6aPZ-M",
+  },
+};
+
+const url = (urlName) => {
+  return baseUrl + `${urlName}?language=ko-kr`;
+};
+
+export const nowPlaying = () =>
+  fetch(url("movie/now_playing"), options).then((res) => res.json());
+
+export const popular = () =>
+  fetch(url("movie/popular"), options).then((res) => res.json());
+
+//! 동기화
+
+//! 비동기화(Promise)
